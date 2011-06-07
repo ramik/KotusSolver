5d905a2fded84fafdcba36b2fa9a76cbed2b98fc
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-06-08 00:47:15 +0300

    importing to github, xml file needs correction

---
+++ Kali/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace Kali
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ResolveKotusResults(object sender, RoutedEventArgs e)
+        {
+            var resolveFromKotus = Module1.resolveFromKotus(textBox1.Text);
+            textBlock1.Text = "";
+            /*for (var i = 0; i < resolveFromKotus.Count(); i++)
+            {
+                textBlock1.Text += resolveFromKotus.ElementAt(i) + ", ";
+                textBlock1.Text += i%8 == 0 ? "\n" : "";
+            }*/
+            int ui = 0;
+
+            var splits = from name in resolveFromKotus
+                         group name by ui++ < 8 into part
+                         select part.AsEnumerable().Aggregate(((x, y) => x + ", " + y));
+            var f = resolveFromKotus.Where((x, i) => i % 8 == 0).Select((x, i) => resolveFromKotus.Skip(i * 8).Take(8));
+            var joined = f.Select(chunk => chunk.Aggregate(((x, y) => x + ", " + y)));
+            foreach (var result in joined)
+            {
+                textBlock1.Text += result + "\n";
+            }
+        
+
+
+            // from x in resolveFromKotus
+            /*while(resolveFromKotus.Any())
+            {
+                var enumerable = resolveFromKotus.Take(5);
+                textBlock1.Text += enumerable.Aggregate((x, y) => x + ", " + y);
+                resolveFromKotus.Skip(5);
+            }
+             */
+        }
+    }
+}
---
+++ KotusHandleri/Module1.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.net
+
+module Module1
+open System.IO
+open System.Xml.Linq
+
+let rec canBeFormed (x : string) (y : string) = 
+            if(x.Length = 0) then true
+            else
+                let found = y.IndexOf(x.Chars(0))
+                if (found = -1) then false
+                else canBeFormed (x.Substring(1)) (y.Remove(found, 1))
+
+let fileparsed = XElement.Load(@"c:\Kotusprojekti\Kali\KotusHandleri\kotus-sanalista_v1.xml") 
+
+let content ( file : XElement) y = 
+                file.Elements(XName.Get("st")) |> Seq.map (fun x -> x.Element(XName.Get("s"))) |> Seq.map (fun x -> x.Value)
+                |> Seq.filter (fun x -> x.Length < 8 && x.Length > 2)
+                |> Seq.filter (fun x -> canBeFormed x y)
+                |> Seq.distinct
+                |> Seq.sortBy (fun x -> x.Length)
+
+let resolveFromKotus x = content fileparsed x
